1f25c794ce080c18677458f4f63abc1afc295689
Juliano Tavares <juliano.tavaress@gmail.com>
2021-02-27 20:01:26 -0300

    run migrations

--- src/database/migrations/1614466100045-CreateUser.ts
+++ src/database/migrations/1614466100045-CreateUser.ts
@@ -1,11 +1,37 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+/* eslint-disable class-methods-use-this */
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateUser1614466100045 implements MigrationInterface {
+export default class CreateUser1614466100045 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  // eslint-disable-next-line class-methods-use-this
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
 }
