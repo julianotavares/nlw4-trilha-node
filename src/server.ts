ac1563ab903930b62c27598d76f4df1e458d8fe5
Juliano Tavares <juliano.tavaress@gmail.com>
2021-02-27 18:50:21 -0300

    added tsc-node-dev

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,14 @@
 import express from 'express';
-import routes from './routes'
+import routes from './routes';
+
 const app = express();
 
-app.listen(3333, () => console.log("Server is running!"))
+app.get('/users', (resquest, response) => {
+  return response.json({ message: 'Hello World - NLW4' });
+});
+
+app.post('/', (resquest, response) => {
+  return response.json({ message: 'Os dados foram salvos com sucesso' });
+});
+
+app.listen(3333, () => console.log('Server is running!'));
